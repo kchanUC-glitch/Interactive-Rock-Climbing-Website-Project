b0725850e56ece16a52b66a8d16246b21ea425d6
kcglitch <chank2@mail.uc.edu>
2026-03-06 22:53:30 -0500

    Added ajax testing method

--- formHandling.php
+++ formHandling.php
@@ -1,6 +1,7 @@
 
 <?php
-#process data received from AJAX in scripts.js and insert it into the database using PHP
+#process data requested from AJAX in scripts.js and return the (for adaptive searchbar)
+#This will also take in the form data from contact.php and update the mySQL DB. MAKE SURE TO VALIDATE.
 
 
 
--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 const myButton = document.getElementById("submitButton")
 
-myButton.addEventListener("click", submitForm) 
+myButton.addEventListener("click", search) 
 
-// use AJAX to submit the form data via php without refreshing the page
-function submitForm() {
-    alert("Form submitted!");
+// use AJAX to submit the form data via php without refreshing the page. Using Jquery.
+function search() {
+    // link to php here
+    $("#submitButton").load("index.php", function () {
+        alert("Ajax performed!");
+    });
 }
